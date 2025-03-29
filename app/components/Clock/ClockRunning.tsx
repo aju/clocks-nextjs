131418aa088c7aec24f469e327bdb93fcfce4cd7
Andrzej Maciurzynski <andrzej.maciurzynski@gmail.com>
2025-03-29 19:51:25 +0100

    fix eslint

--- app/components/Clock/ClockRunning.tsx
+++ app/components/Clock/ClockRunning.tsx
@@ -1,20 +1,10 @@
 'use client'
 
-import React, { useEffect } from 'react';
+import React from 'react';
 import Clock from './Clock';
 import useInterval from '@/app/helpers/useInterval';
 
 export default function ClockRunning() {
-    useEffect(() => {
-        console.log(
-            'Dashboard Component is being mounted! ################################',
-        );
-
-        return () => {
-            console.trace();
-            console.log('Dashboard Component is being unmounted!');
-        };
-    }, []);
     const currentDate = new Date();
     const [time, setTime] = React.useState(`${currentDate.getHours()}:${currentDate.getMinutes()}:${currentDate.getSeconds()}}`);
 
